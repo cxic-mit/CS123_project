00019dfabd0328f48470bc13a295771c8b88eff4
Rex Yu <rexyu@outlook.com>
2018-05-25 13:05:12 -0500

    fixed a bug and improved the performance

--- FriendsRecommender.py
+++ FriendsRecommender.py
@@ -1,6 +1,7 @@
 from mrjob.job import MRJob
 import itertools
 from heapq import nlargest
+import time
 
 TOP_N = 5 #to reduce the file size
 
@@ -15,12 +16,12 @@ class FriendsRecommender(MRJob):
         '''
         user, friends_str = line.split(':')
         friends = friends_str.split(',')
-        if friends == 'private' or friends == 'notfound':
+        if 'private' in friends or 'notfound' in friends:
             return
 
         pairs = list(itertools.permutations(friends, 2))
         for pair in pairs:
-            yield pair, user
+            yield str(pair[0]) + ' ' + str(pair[1]), user
 
     def combiner(self, pair, friends):
         '''
@@ -30,14 +31,12 @@ class FriendsRecommender(MRJob):
         Output: key-value pair with unique key representing two people and all friends in common
                 e.g. user1, (user2, [mutualFriend1, mutualFriend2, ...])
         '''
+        user1, user2 = pair.split(' ')
         if friends:
             count = 0
             for f in friends:
                 count += 1
-            yield pair[0], (pair[1], count)
-
-    def reducer_init(self):
-        self.dict = {}
+            yield user1, user2 + ' ' + str(count)
 
     def reducer(self, user, friends):
         '''
@@ -49,12 +48,11 @@ class FriendsRecommender(MRJob):
         Output: ranked list of TOP_N potential friends
         '''
         if friends:
-            self.dict[user] = nlargest(TOP_N, friends, key = lambda e:e[1])
+            yield user, nlargest(TOP_N, friends, key = lambda e:int(e.split(' ')[1]))
 
-    def reducer_final(self):
-        for user, friend_list in self.dict.items():
-            friend_list.sort(reverse = True)
-            yield user, friend_list
 
 if __name__ == '__main__':
+    start = time.clock()
     FriendsRecommender.run()
+    end = time.clock()
+    print("time:", end - start)
