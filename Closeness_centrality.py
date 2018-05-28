db2dc57e076cd3f4c7044506a075d3333acee563
Xi Chen <32280618+uc-xic@users.noreply.github.com>
2018-05-28 17:38:35 -0500

    Add files via upload

---
+++ Closeness_centrality.py
@@ -0,0 +1,74 @@
+import numpy as np
+from mrjob.job import MRJob
+import re
+
+def construct_distance_dict(data):
+    user_dict = {}
+
+    for line in data:
+        cnt=0
+        line = re.findall(r"[-+]?\d*\.\d+|\d+", line)
+        user_1 = line[0]
+        distance_dict = {}
+        
+        for i in range(1,len(line)-1):
+            cnt+=1
+            if cnt%2==1:
+                user_2 = line[i]
+                distance = line[i+1]
+            
+            distance_dict[user_2] = distance
+
+        user_dict[user_1] = distance_dict
+
+    return user_dict
+
+
+class Closeness_centrality(MRJob):
+
+    def mapper_init(self):
+
+        # with open("output_distance_0-10000.txt", "r") as f: 
+        with open("output_distance_200-500.txt", "r") as f:   
+            data = f.readlines()
+
+        self.distance_dict = construct_distance_dict(data)
+
+
+    def mapper(self, _, line):
+
+        center, community = line.split('\t')
+        center = center.strip('""')
+
+        community = community.strip('[]').split(',')
+        n = len(community)
+
+        for member in community:
+            member_dict = self.distance_dict[member]
+            closeness = 0.0
+
+            for member_copy in community:
+
+                closeness += float(member_dict[member_copy])
+            
+            closeness_centrality = (n-1) / closeness
+
+            yield center, (member, round(closeness_centrality, 4)) 
+
+   
+    def reducer(self, center, closeness_tuple): 
+        
+        closeness_dict = {}
+
+        for i in closeness_tuple:
+            member = i[0]
+            centrality = i[1]
+            closeness_dict[member] = centrality
+
+        most_important = max(closeness_dict, key = closeness_dict.get)
+
+        yield center, (most_important, closeness_dict[most_important])
+
+
+if __name__ == '__main__':
+    Closeness_centrality.run()
---
+++ Eigenvector_centrality.py
@@ -0,0 +1,71 @@
+import numpy as np
+from mrjob.job import MRJob
+import re
+from numpy import linalg as LA
+
+
+def read_line(line):
+    user, friends_str = line.split(':')
+    friends = friends_str.split(',')
+    return user, friends
+
+
+class Eigenvector_centrality(MRJob):
+
+    def mapper_init(self):
+
+        with open("data_200-500.txt", "r") as f:
+            self.data = f.readlines()
+
+
+    def mapper(self, _, line):
+
+        center, community = line.split('\t')
+        center = center.strip('""')
+
+        community = community.strip('[]').split(',')
+        n = len(community)
+
+        a_matrix = np.zeros(shape = (n,n))   
+
+        for member in community:
+
+            for line in self.data:
+                user, friends = read_line(line)
+                
+                if friends != ['private\n'] and \
+                   friends != ['notfound\n'] and \
+                   friends != ['\n']:
+
+                   for i in range(n):
+                    if community[i] == user:
+                        for j in range(n):
+                            if community[j] in friends:
+                                a_matrix[i, j] = 1
+
+        w, v = LA.eig(a_matrix)
+        w = list(w)
+        max_index = w.index(max(w))
+                     
+        for i in range(n):
+            eigenvector = round(v[max_index][i], 4)
+            eigenvector_centrality = eigenvector.real
+
+            yield center, (community[i], eigenvector_centrality)
+                
+
+    def reducer(self, center, eigenvector_tuple): 
+        eigenvector_dict = {}
+
+        for i in eigenvector_tuple:
+            member = i[0]
+            centrality = i[1]
+            eigenvector_dict[member] = centrality
+
+        most_important = max(eigenvector_dict, key = eigenvector_dict.get)
+
+        yield center, (most_important, eigenvector_dict[most_important])
+        
+        
+if __name__ == '__main__':
+    Eigenvector_centrality.run()
