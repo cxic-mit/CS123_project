03f12debea8086f8471ae3cb6be01f21915d6872
Rose Gao <rosegao@cs.uchicago.edu>
2018-05-29 17:21:34 -0500

    well connected

--- most_well_connected_person.py
+++ most_well_connected_person.py
@@ -20,19 +20,23 @@ def preprocess_data(input_file, preprocess_file):
                 outStr = '|'.join([userID, edges, str(distance), path, color])
                 p.write(outStr)
                 p.write('\n')
-    f.close()
+        f.close()
     p.close()
 
-
-def write_results(preprocess_file, output_file, START_NODE, end_nodes):
-    with open(preprocess_file) as f, open(output_file) as out:
+def write_results(preprocess_file, output_file, START_NODE, END_NODE, N):
+    flag = False
+    with open(preprocess_file) as f, open(output_file, 'w+') as out:
         for line in f:
             line = line.strip('\n')
             fields = line.split('|')
-            for end_node in end_nodes:
-                if str(fields[0]) == end_node and int(fields[-3]) < 9999:
-                    print('write line')
-                    out.write('{} {} {}'.format(START_NODE, end_node, fields[-3]))
+            if str(fields[0]) == END_NODE and int(fields[-3]) < 9999:
+                flag = True
+                print('The path from {0} to {1} is {2}, the distance is {3}'.format(START_NODE, END_NODE, \
+                    '->'.join(fields[-2].split()) + '->' + str(END_NODE), fields[-3]))
+                out.write(START_NODE, END_NODE, fields[-3])
+
+    if not flag:
+        print('Cannot find the path from {0} to {1} within {2} degrees.'.format(START_NODE, END_NODE, N))
 
 def main():
     input_file = './data/friends-000______small.txt'
@@ -41,22 +45,20 @@ def main():
 
     preprocess_data(input_file, preprocess_file)
 
-    N_iterations = 10
-    START_NODE = 102
+    N = 10
+    START_NODE = 146
     nodes = 10
-    end_nodes = range(START_NODE, START_NODE+nodes)
+    end_nodes = range(START_NODE+1, START_NODE+nodes+1)
 
-    for end_node in end_nodes:
-        args = '--start_node {0} --end_node {1} {2} --output=results'.format(START_NODE, end_node, preprocess_file)
-        print(args)
+    for END_NODE in end_nodes:
+        args = '--start_node {0} --end_node {1} {2} --output=results'.format(START_NODE, END_NODE, preprocess_file)
         mr_job = MRBFS(args=args.split())
 
-        for i in range(N_iterations):
+        for i in range(N):
             with mr_job.make_runner() as runner:
                 runner.run()
-            os.system('cat results/* > {0}'.format(preprocess_file))
 
-    #write_results(preprocess_file, output_file, START_NODE, end_nodes)
+        write_results(preprocess_file, output_file, START_NODE, END_NODE, N)
 
 if __name__== "__main__":
     main()
