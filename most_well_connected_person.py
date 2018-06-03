9e7374402839a9c3f3fe1a52fa3839437f94b3e5
Rose Gao <rose.gao6@gmail.com>
2018-06-03 10:00:02 -0500

    simplified most well connected

--- most_well_connected_person.py
+++ most_well_connected_person.py
@@ -1,59 +1,59 @@
 # We define most well connected person as the node with the minimum eccentricity,
 # a.k.a. the graph radius.
 
-import sys
-import os
 import run_MRBFS
 from MRMinEccentricity import MRMinEccentricity
 
+def get_good_nodes(input_file):
+	"""
+	Return a list of nodes that aren't private or notfound or ''
+	"""
+	nodes = []
 
-def write_results(output_file, paths_file, start_node):
-	"""
-	Find paths and write viable path distance to an output file.
-	Inputs:
-		preprocessed_file: input file
-		paths_file: output file
-		start-node: start node
-		end_nodes: list of end nodes
-		N: degrees limitation
-	Returns: no explicit return; writes to paths_file.
-	"""
-	print('writing...')
-	with open(output_file) as f, open(paths_file, 'a') as p:
+	with open(input_file) as f:
 		for line in f:
 			line = line.strip('\n')
-			fields = line.split('|')
-			distance = int(fields[-3])
-			end_node = str(fields[1])
-			if distance < 9999:
-				print('Found path!', start_node, end_node, distance)
-				outStr = '|'.join([start_node, end_node, str(distance)])
-				p.write(outStr)
-				p.write('\n')
+			fields = line.split(':')
+			if '' not in fields and 'private' not in fields \
+					and 'notfound' not in fields:
+				nodes.append(fields[0])
+
+	return nodes
+
+
+def run_mrjob(mr_job, output_file, min_file):
+	"""
+	Run MRMinEccentricity
+	"""
+	for i in range(N):
+		with mr_job.make_runner() as runner:
+			runner.run()
+		os.system('cat results/* > {0}'.format(min_file))
 
 
 def main():
-	"""
-	Iterate through every pair of nodes to find distances.
-	Find the node with the minimum eccentricity.
-	"""
-	output_file = './results/paths.txt'
-	min_file = './results/min.txt'
+    """
+    Iterate through every pair of nodes to find distances.
+    Find the node with the minimum eccentricity.
+    """
+    input_file = './data/friends-000______small.txt'
+    output_file = './results/paths.txt'
+    min_file = './results/min.txt'
 
-	nodes = range(102, 1000)
-
-	for i in nodes:
-		for j in nodes:
-			if i != j:
-				start_node = str(i)
-				end_node = str(j)
-				print(start_node, end_node)
-				line = run_MRBFS.main(start_node, end_node)
+    nodes = get_good_nodes(input_file)[:100]
+    for i in nodes:
+        for j in nodes:
+            if i != j:
+				print(i, j)
+				line = run_MRBFS.main(i, j)
 				if line:
 					with open(output_file, 'a') as f:
 						f.write(line)
 						f.write('\n')
 
+# args = '--jobconf mapreduce.job.reduces=1  ----{} --{}'.format(output_file. min_file)
+# mr_job = MRBFS(args=args.split())
+# run_mrjob(mrjob, output_file, min_file)
 
 if __name__ == "__main__":
 	main()
