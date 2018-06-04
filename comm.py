50e28d4ec85c036df6723bb1c6159cd1cf781dbf
yyd007 <32532641+yyd007@users.noreply.github.com>
2018-06-03 22:06:48 -0500

    Update comm.py

--- comm.py
+++ comm.py
@@ -1,10 +1,14 @@
-# The output of this community is userA, [(user_1, dist1),...(user_10, dist10)]
-
+# The output of this community is user1, [(user_1, dist1),...(user_50, dist50)]
+# This file takes in the output file from Get_pairs_distance.py and returns
+# the 50 users within the community along with their distances to the user1
 from mrjob.job import MRJob
 import math
 
 
 def read_line(line):
+# This function reads in each line of the input file from Get_pairs_distance.py
+# and returns each userN, and a list of other users and their distances
+# to userN
     cnt=0
     line = re.findall(r"[-+]?\d*\.\d+|\d+", line)
     user_1 = line[0]
@@ -23,7 +27,8 @@ def read_line(line):
 
 
 class find_comm(MRJob):
-
+# This mapreduce function returns each user and his corresponding friends community
+# of size 50
     def mapper(self, _, line):
         ppl = []                   
         user, lst = read_line(line)       
